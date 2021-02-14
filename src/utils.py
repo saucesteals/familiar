3c0e478b677c1c6c32aab37a338fd38ff8e002f5
daniel <saucesteals@gmail.com>
2021-02-13 20:10:40 -0800

    fix

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import discord
 
+def get_member_name(member:discord.Member) -> str:
+    return member.nick if member.nick else member.name
 
 def cleanse_prompt(prompt:str, message:discord.Message) -> str:
     for mention in message.mentions:
-        prompt = prompt.replace(f"<@!{str(mention.id)}>", mention.nick if mention.nick else mention.name)
+        prompt = prompt.replace(f"<@!{str(mention.id)}>", get_member_name(mention))
 
     for channel in message.channel_mentions:
         prompt = prompt.replace(f"<#{str(channel.id)}>", channel.name)
@@ -12,7 +14,3 @@ def cleanse_prompt(prompt:str, message:discord.Message) -> str:
         prompt = prompt.replace(f"<@&{str(role.id)}>", role.name)
 
     return prompt.strip()
-
-
-def get_member_name(member:discord.Member) -> str:
-    return member.nick if member.nick else member.name
