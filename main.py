a42f97445c1eae1294115ca0975f8c475c1a5bc7
Daniel <saucesteals@gmail.com>
2021-12-10 16:59:42 -0800

    .

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ CUSTOM_DISCORD_PREFIX = os.getenv("DISCORD_BOT_PREFIX")
 OPENAI_ENGINE = os.getenv("OPENAI_ENGINE")
 
 client = FamiliarBot(command_prefix=CUSTOM_DISCORD_PREFIX.strip() if CUSTOM_DISCORD_PREFIX else "!", 
-                     intents=discord.Intents.all(), 
+                     intents=discord.Intents.default(), 
                      openai_api_key=OPENAI_API_KEY.strip(), 
                      openai_engine=OPENAI_ENGINE.strip(),
                      logging=True,
